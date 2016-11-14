f1ddd7edf93a08787d769c0d5aea9f363619ecea
Zhang Chen <zhangchen.shaanxi@gmail.com>
2016-11-14 15:32:54 +0800

    Add a server and a client

---
+++ vmi_client.py
@@ -0,0 +1,46 @@
+# @Time    : 2016/11/14 15:22
+# @Author  : Zhang Chen
+# @Email    : zhangchen.shaanxi@gmail.com
+# @File    : vmi_client.py
+
+#!/usr/bin/env python
+import pika
+import uuid
+
+class VMIRpcClient(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+                host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, instance_name):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='rpc_queue',
+                                   properties=pika.BasicProperties(
+                                         reply_to = self.callback_queue,
+                                         correlation_id = self.corr_id,
+                                         ),
+                                   body=str(instance_name))
+        while self.response is None:
+            self.connection.process_data_events()
+        return int(self.response)
+
+vmi_rpc = VMIRpcClient()
+
+instance_name = "instance-00000014"
+print(" [x] Requesting get_process_list(%s)",instance_name)
+response = vmi_rpc.call(instance_name)
+print(" [.] Got %s" % response)
---
+++ vmi_server.py
@@ -0,0 +1,56 @@
+# @Time    : 2016/11/14 15:11
+# @Author  : Zhang Chen
+# @Email    : zhangchen.shaanxi@gmail.com
+# @File    : vmi_server.py.py
+
+#!/usr/bin/env python
+import pika
+from oslo_log import log
+import subprocess
+
+LOG = log.getLogger(__name__)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='rpc_queue')
+
+
+def get_process_list(instance_name):
+    command = "vol.py -l vmi://" + instance_name + " --profile=LinuxCentos7-3_10_0-327_36_3_el7_x86_64x64 linux_pslist"
+    LOG.debug('Command: %(command)s',
+              {'command': command})
+    try:
+        out = subprocess.check_output("export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH && " + command,
+                                      shell=True).decode('utf-8')
+    except subprocess.CalledProcessError as e:
+        out_bytes = e.output  # Output generated before error
+        code = e.returncode  # Return code
+        LOG.error(
+            "Error when get process, the output generated before error:%(out_bytes)s, the return code is %(return_code)s",
+            {'out_bytes': out_bytes,
+             'return_code': code})
+
+    LOG.debug('Out: %(out)s',
+              {'out': out})
+    raw_list = out.split("\n")
+    return raw_list
+
+def on_request(ch, method, props, body):
+    instance_name = str(body)
+    response = get_process_list(instance_name)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(on_request, queue='rpc_queue')
+
+LOG.info(" [x] Awaiting RPC requests")
+channel.start_consuming()
