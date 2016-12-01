a2c47ab88f5b99abba8a5437f614d9d9fb5d59d2
Zhang Chen <zhangchen.shaanxi@gmail.com>
2016-12-01 11:56:45 +0800

    add a shutdown.sh add a vmi-server for systemctl add create-service-and-run.sh

---
+++ create-service-and-run.sh
@@ -0,0 +1,11 @@
+# @Time    : 2016/12/1 11:44
+# @Author  : Zhang Chen
+# @Email    : zhangchen.shaanxi@gmail.com
+# @File    : create-service-and-run.sh
+
+chmod a+x vmi-server
+cp vmi-server /etc/init.d/
+cd /etc/init.d/
+chkconfig --add vmi-server
+chkconfig vmi-server on
+systemctl restart vmi-server.service
